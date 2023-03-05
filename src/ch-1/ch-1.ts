8c3b3e7d24795dc1b8a02e7269fed30f6d52391f
lusatiro <ljsatiro@gmail.com>
2023-03-05 09:07:00 -0300

    test: add unit test

--- src/ch-1/ch-1.ts
+++ src/ch-1/ch-1.ts
@@ -19,5 +19,21 @@ function arrayManipulation(N: number, queries: number[][]) {
 }
 
 console.time("start");
+assert.strictEqual(
+    arrayManipulation(5, [
+        [1, 2, 100],
+        [2, 5, 100],
+        [3, 4, 100],
+    ]),
+    200
+);
+assert.strictEqual(
+    arrayManipulation(4, [
+        [2, 3, 603],
+        [1, 1, 286],
+        [4, 4, 882],
+    ]),
+    882
+);
 assert.strictEqual(arrayManipulation(10000000, data), 2497169732);
 console.timeEnd("start");
